7e738ec82204280ea999a0101446e40235c29512
JinyangSSS <121841021+JinyangSSS@users.noreply.github.com>
2025-08-21 09:49:39 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cww5ivbhoyr4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "00000000"
+customdb = "employee"
+custombucket = "sim-employee"
+customregion = "us-east-1b"
+
